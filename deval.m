866e88171d71f0663511b5721d3bd0bbf0ea5e13
Rody Oldenhuis <oldenhuis@gmail.com>
2014-07-02 16:56:07 +0200

    Improved deval() stub by only initialzing the function handle to the built-in deval() once, instead of on each call.

--- deval.m
+++ deval.m
@@ -45,6 +45,14 @@ function [Sxint,Spxint] = deval(sol,xint,idx)
 % If you find this work useful, please consider a donation:
 % https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6G3S5UYM7HJ3N
     
+    persistent deval_original
+    if isempty(deval_original)
+        prevPath = pwd;
+        cd([matlabroot filesep 'toolbox' filesep 'matlab' filesep 'funfun']);
+        deval_original = @deval;
+        cd(prevPath);
+    end
+
     if ~isa(sol, 'struct')
         % Try DEVAL(XINT,SOL)
         temp = sol;
@@ -72,11 +80,7 @@ function [Sxint,Spxint] = deval(sol,xint,idx)
             'differential equation solver.']);        
     end
     
-    % Delegate if solver is not RKN1210
-    prevPath = pwd;
-    cd([matlabroot filesep 'toolbox' filesep 'matlab' filesep 'funfun']);
-    deval_original = @deval;
-    cd(prevPath);      
+    % Delegate if solver is not RKN1210       
     switch solver
         
         %case 'rkn1210'
